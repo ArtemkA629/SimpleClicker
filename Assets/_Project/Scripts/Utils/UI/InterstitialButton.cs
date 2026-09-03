9357704bfda59193abfe462c284e4e3c979b0247
ArtemkA629 <77196829+ArtemkA629@users.noreply.github.com>
2026-09-03 13:11:29 +0300

    interstitial added

---
+++ Assets/_Project/Scripts/Utils/UI/InterstitialButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using YG;
+
+public class InterstitialButton : MonoBehaviour
+{
+    private Button _button;
+    
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+    }
+
+    private void OnEnable()
+    {
+        _button.onClick.AddListener(OnButtonClick);
+    }
+
+    private void OnDisable()
+    {
+        _button.onClick.RemoveListener(OnButtonClick);
+    }
+    
+    private void OnButtonClick()
+    {
+        YG2.InterstitialAdvShow();
+    }
+}
